73fa422bc1c17d28dd13e293fecabb2a8cd51beb
Somiichan <cupichan_93@hotmail.com>
2023-04-12 17:13:34 +0200

    pendiente retocar css - estudiar mejor el js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,148 @@
+class Calculator {
+	
+	constructor() {
+		this.current = 0;
+		this.entered = 0;
+		this.answer = 0;
 
-let result = document.getElementById("result");
+		this.decimal = false;
 
-    function addToResult(value) {
-    result.value += value;
+		this.operator = '';
+
+		this.states = {
+			'inv': false,
+			'comma': false
+		}
+
+		this.options = [
+			['equals', this.processEquals.bind(this)],
+			['clear', this.processClear.bind(this)],
+			['sqrt', this.processSqrt.bind(this)],
+			['inv', this.processInv.bind(this)],
+			['comma', this.processComma.bind(this)],
+			['pi', this.processPi.bind(this)]
+		];
+
+		this.firstEnteredOutput = document.querySelector('.f_entered');
+		this.lastEnteredOutput = document.querySelector('.l_entered');
+		this.operatorOutput = document.querySelector('.operator');
+
+		this.currentOutput = document.querySelector('.current > h1');
+		this.buttons = document.querySelectorAll('.buttons > div');
+
+		for(var i = 0, n = this.buttons.length; i < n; i++) {
+			var calc = this;
+			var button = this.buttons[i];
+
+			button.addEventListener('mousedown', function() {
+				var _this = this;
+				calc.processAction(_this.getAttribute('data-key'));
+				_this.classList.add('pressed');
+				setTimeout(function() {
+					_this.classList.remove('pressed');
+				}, 400);
+			});
+
+			button.addEventListener('mouseup', function() {
+				var _this = this;
+				_this.classList.remove('pressed');
+			});
+		}
+	}
+
+	processAction(a) {
+		for(var i = 0, n = this.options.length; i < n; i++) {
+			var option = this.options[i];
+			if(a === option[0]) {
+				option[1]();
+				return;
+			}
+		}
+
+		if(a === '+' || a === '-' || a === '/' || a === '*') {
+			this.processASDM(a);
+			return;
+		} else {
+			this.processNumber(a);
+			return;
+		}
+
+	}
+
+	processEquals() {
+		if(!!this.operator) {
+			this.displayNumber(this.current, this.lastEnteredOutput);
+			this.answer = eval(this.entered + this.operator + this.current);
+			this.displayNumber(this.answer, this.currentOutput);
+
+			this.current = this.answer;
+		}
+	}
+
+	processClear() {
+		this.current = 0;
+		this.displayNumber(this.current, this.currentOutput);
+		this.entered = 0;
+		this.operator = '';
+		this.firstEnteredOutput.innerHTML = '';
+		this.lastEnteredOutput.innerHTML = '';
+		this.operatorOutput.innerHTML = '';
+	}
+
+	processSqrt() {
+		this.current = Math.sqrt(this.current);
+		this.displayNumber(this.current, this.currentOutput);
+	}
+
+	processInv() {
+		this.current = this.current * -1;
+		this.displayNumber(this.current, this.currentOutput);
+	}
+
+	processComma() {
+		if(!this.decimal) {
+			this.current += '.';
+			this.currentOutput.innerHTML = this.current;
+		}
+
+		this.decimal = true;
+	}
+
+	processPi() {
+		this.current = Math.PI;
+		this.displayNumber(this.current, this.currentOutput);
+	}
+
+	processASDM(a) {
+		if(!!this.entered && !!!this.answer) {
+			return;
+		}
+
+		if(this.answer) {
+			this.lastEnteredOutput.innerHTML = '';
+		}
+
+		this.decimal = false;
+		this.operator = a;
+		this.entered = this.current;
+		this.displayNumber(this.entered, this.firstEnteredOutput);
+		a === '*' ? this.operatorOutput.innerHTML = 'x' : this.operatorOutput.innerHTML = this.operator;
+		this.current = 0;
+		this.displayNumber(this.current, this.currentOutput);
+	}
+
+	processNumber(n) {
+    if (this.current === 0) {
+        this.current = n;
+    } else if (this.current.toString().length < 10) {
+        this.current += n;
+    }
+    this.displayNumber(this.current, this.currentOutput);
     }
 
-    function clearResult() {
-    result.value = "";
-    }
+	displayNumber(n, location) {
+		location.innerHTML = String(n).substring(0, 10);
+	}
+}
 
-    function backspace() {
-    result.value = result.value.slice(0, -1);
-    }
-
-    function calculate() {
-    try {
-        result.value = eval(result.value);
-    } catch (error) {
-        result.value = "Error";
-    }
-    }
+var Calc = new Calculator();
